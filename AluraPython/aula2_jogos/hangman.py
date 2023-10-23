2c62e5f4b43a98e6d6f4ace7a569c7c9003e9164
Rougert Brian Alexandre <rougert.brian@gmail.com>
2023-10-23 06:34:36 -0300

    Checagens e aprimoramentos no código

--- AluraPython/aula2_jogos/hangman.py
+++ AluraPython/aula2_jogos/hangman.py
@@ -6,16 +6,40 @@ def play():
 	print("Welcome to Hangman!")
 	print("****************************")
 
-	lista = ["banana", "maca", "uva", "pera"]
-	secret_word = random.choice(lista)
-	
-	correct_awnser = False
-	died = False
+	lista = ["Banana", "Maca", "Uva", "Pera"]
+	secret_word = random.choice(lista).lower()
+	correct_guesses = ["_" for _ in secret_word]  # _ = a not used variable
+	wrong_guesses = 5
 
-	while not correct_awnser or  not died:
-		print("jogando")
+	right_awnser = "_" not in correct_guesses
+	died = wrong_guesses == 0
 
-	print("GAME OVER")
+	while not right_awnser and not died:
+		print(f"You have {wrong_guesses} guesses")
+		while True:
+			guess = input("Enter a letter: ").lower().strip()
+			if len(guess) > 1 or guess.isdecimal() or guess in correct_guesses:
+				print("Invalid entry, try again")
+			else:
+				break
+		index = 0
+		for letter in secret_word:
+			if guess == letter:
+				correct_guesses[index] = letter
+			index += 1
+		if guess not in secret_word:
+			wrong_guesses -= 1
+
+		print(correct_guesses)
+
+		right_awnser = "_" not in correct_guesses
+		died = wrong_guesses == 0
+
+
+	if died:
+		print("You lose!")
+	else:
+		print("Congrats, you won!")
 
 
 if __name__ == "__main__":
