a6e989935af1e89aaec165d33fca415587c6ec39
Rougert Brian Alexandre <rougert.brian@gmail.com>
2023-10-27 23:50:33 -0300

    Atributos privados e exercícios

--- AluraPython/curso_3_POO/account.py
+++ AluraPython/curso_3_POO/account.py
@@ -1,20 +1,20 @@
 class Account:
 	def __init__(self, number, owner, balance, limit = 10000) -> None:
-		self.number = number
-		self.owner = owner
-		self.balance = balance
-		self.limit = limit
+		self.__number = number
+		self.__owner = owner
+		self.__balance = balance
+		self.__limit = limit
 
 	def extract(self):
-		print(f"Your balance is {self.balance}, Mr/Ms.{self.owner}")
+		print(f"Your balance is {self.__balance}, Mr/Ms.{self.__owner}")
 
 	def deposit(self, value):
-		self.balance += value
+		self.__balance += value
 	
 	def withdraw(self, value):
-		self.balance -= value
+		self.__balance -= value
 
 
 	def __str__(self) -> str:
-		string = f"Number: {self.number}\nOwner: {self.owner}\nBalance: {self.balance}\nLimit: {self.limit}\n"
+		string = f"Number: {self.__number}\nOwner: {self.__owner}\nBalance: {self.__balance}\nLimit: {self.__limit}\n"
 		return string
---
+++ AluraPython/curso_3_POO/exercises/test.py
@@ -0,0 +1,4 @@
+from date import Date
+
+d = Date(21, 11, 2007)
+d.format_date()
