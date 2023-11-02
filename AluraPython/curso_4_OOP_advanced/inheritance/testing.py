3752734871c5de6f67e21f61e604bdb8b2d69458
Rougert Brian Alexandre <rougert.brian@gmail.com>
2023-11-02 20:43:55 -0300

    Polimofismo

--- AluraPython/curso_4_OOP_advanced/inheritance/testing.py
+++ AluraPython/curso_4_OOP_advanced/inheritance/testing.py
@@ -10,3 +10,9 @@ atlanta.new_like()
 
 avangers.show_info()
 atlanta.show_info()
+
+movies_series = [avangers, atlanta]
+
+for program in movies_series:
+	detail = program.duration if hasattr(program, "duration") else program.seasons
+	print(f"{program.name} - {detail} - {program.likes}")
