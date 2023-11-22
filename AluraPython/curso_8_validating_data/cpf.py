1faff75cf904ca968335bcf143b0f6a2890df448
Rougert Brian Alexandre <rougert.brian@gmail.com>
2023-11-22 08:16:55 -0300

    Validando CPF utilizando a biblioteca validate_docbr

---
+++ AluraPython/curso_8_validating_data/cpf.py
@@ -0,0 +1,35 @@
+import re
+from validate_docbr import CPF
+
+class Cpf:
+	
+	def __init__(self, doc) -> None:
+
+		if self.valid_cpf(doc):
+			self.cpf = doc
+		else:
+			raise ValueError("Invalid CPF")
+	
+	def valid_cpf(self, doc):
+		# cpf_pattern = re.compile("\d{3}.\d{3}.\d{3}-\d{2}")
+		# match = cpf_pattern.match(doc)
+		
+		if len(doc) == 11:
+			validating = CPF()
+			return validating.validate(doc)
+		else:
+			raise ValueError("Wrong number of digits")
+
+	def format_cpf(self):
+		# cpf_part1 = self.cpf[:3]
+		# cpf_part2 = self.cpf[3:6]
+		# cpf_part3 = self.cpf[6:9]
+		# cpf_part4 = self.cpf[9:]
+		# formated_cpf = f"{cpf_part1}.{cpf_part2}.{cpf_part3}-{cpf_part4}"
+
+		formated_cpf = CPF().mask(self.cpf)
+
+		return formated_cpf
+
+	def __str__(self) -> str:
+		return self.format_cpf()
---
+++ AluraPython/curso_8_validating_data/testing.py
@@ -0,0 +1,6 @@
+from cpf import Cpf
+# from validate_docbr import CPF # pip install validate_docbr
+
+cpf1 = Cpf("94695916132")
+
+print(cpf1)
